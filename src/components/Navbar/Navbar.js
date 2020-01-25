25b33f9278d600a5098fded2f6ca29ec47acd34e
mohak92 <tamhane.mohak@gmail.com>
2020-01-25 08:06:23 -0500

    Navber component implementation

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar(props) {
+    return (
+        <nav className="navbar d-flex justify-content-between px-5">
+            <span><a href="/">Clicky Game</a></span>
+            <span>{props.msg}</span>
+            <span>Score: {props.score} | Top Score: {props.topScore}</span>
+        </nav>
+    );
+}
+
+export default Navbar;
