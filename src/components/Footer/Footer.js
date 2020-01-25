d827b2112b62ace048fcab5110a54b0bcc1ed4b8
mohak92 <tamhane.mohak@gmail.com>
2020-01-25 08:36:09 -0500

    Footer component implementation

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+
+        <div className="footer">
+            <p className="center">Clicky Game &copy;2020</p>
+        </div>
+    );
+}
+
+export default Footer;
