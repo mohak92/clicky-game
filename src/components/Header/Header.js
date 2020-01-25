7b169ef919487262dc3f4874b23e2407cd899700
mohak92 <tamhane.mohak@gmail.com>
2020-01-25 08:38:43 -0500

    header component render

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Clicky Game!</h1>
+            <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+        </div>
+    );
+}
+export default Header;
