5d5d2928a74b27770a6917077114f859632c8647
mohak92 <tamhane.mohak@gmail.com>
2020-01-25 08:02:40 -0500

    init with state and navbar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      Clicky Game
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    currentScore: 0,
+    topScore: 0,
+    selectedImages: [],
+    gameMsg: "Click on a image to begin!"
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar
+          msg={this.state.gameMsg}
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
