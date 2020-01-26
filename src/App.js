23e4ed5573f8eb998362321a389ff2083687721f
mohak92 <tamhane.mohak@gmail.com>
2020-01-26 12:22:51 -0500

    render image inside a container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Images from './components/Images/Images';
+import games from "./games.json";
+import Container from './components/Container/Container';
 
 
 class App extends Component {
@@ -10,18 +13,34 @@ class App extends Component {
     currentScore: 0,
     topScore: 0,
     selectedImages: [],
-    gameMsg: "Click on an image to begin!"
+    gameMsg: "Click on an image to begin!",
+    games
   };
+
+  imageClicked = (id) => {
+
+  }
 
   render() {
     return (
-      <div className="App" style={{height:"1500px",}}>
+      <div className="App">
         <Navbar
           msg={this.state.gameMsg}
           score={this.state.currentScore}
           topScore={this.state.topScore}
         />
         <Header />
+        <Container>
+        {games.map(city => (
+            <Images
+              key={city.id}
+              id={city.id}
+              name={city.name}
+              image={city.image}
+              imageClicked={this.imageClicked}
+            />
+          ))}
+         </Container>
         <Footer />
       </div>
     );
