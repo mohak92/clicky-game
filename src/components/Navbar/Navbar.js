88ce10f0f2f2f2cf08c4e345a739a4d986aa932f
Mohak Tamhane <tamhane.mohak@gmail.com>
2020-01-27 09:52:41 -0500

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar(props) {
     return (
         <nav className="navbar d-flex justify-content-between px-5">
-            <span><a href="/clicky-game">Clicky Game</a></span>
+            <span><a href="/">Clicky Game</a></span>
             <span>{props.msg}</span>
             <span>Score: {props.score} | Top Score: {props.topScore}</span>
         </nav>
